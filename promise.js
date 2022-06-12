22fd6850932aed5f18585eb26a39decdcb5f2cbb
[agung permana] <[agung.ten@gmail.com]>
2022-06-12 19:48:40 +0700

    promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,38 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let jmlTotal = 0;
+        const IXX = await promiseTheaterIXX()
+                          .then(res => {
+                            let jml = 0;
+                            res.forEach(el => {
+                              if(el.hasil === emosi) {
+                                jml = jml + 1;
+                              }
+                            });
+                            return jml;
+                          });
+        const VGC = await promiseTheaterVGC()
+                        .then(res => {
+                          let jml = 0;
+                          res.forEach(el => {
+                            if(el.hasil === emosi) {
+                              jml = jml + 1;
+                            }
+                          });
+                          return jml;
+                        });;
+
+        jmlTotal = IXX + VGC;
+        resolve(jmlTotal);
+      } catch (error) {
+        reject(error);
+      }
+    })
+}
 
 module.exports = {
   promiseOutput,
